83a44ab545289c90671d919165487c26c9ed7387
Mohammed Ashraf <ashrafKacper555@gmail.com>
2021-11-17 23:26:02 +0530

    Add files via upload
    Array program to find Maximum and Minimum value of array elements.

---
+++ 2)MaxMin.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+class MaxMin
+{
+public static void main(String args[])
+{
+int arr[] = {1,2,3,4,5};
+int max,min;
+Arrays.sort(arr);
+System.out.println("Max : " + arr[4]);
+System.out.println("Min : " + arr[0]);
+
+
+}
+}
